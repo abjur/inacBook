70f58106afa19d86aba3154fbc07a242da357557
Julio Trecenti <julio.trecenti@gmail.com>
2017-02-20 12:58:14 -0300

    function template

---
+++ R/eixo01.R
@@ -0,0 +1,15 @@
+#' Meta: template para documentação das funcoes
+#'
+#' Funções univariadas
+#' <tipo>_<var>(d, parms)
+#'
+#' tipo: tab_ ou graf_: tabela ou gráfico
+#' var: nome de uma coluna de tidy_cnc
+#' d: base de dados (filtro de tidy_cnc)
+#' parms: pode ser ... ou nomes de parametros especificos e documentados
+
+
+#' Tabela de valores de ressarcimento
+tab_vl_ressarcimento <- function(d, ...) {
+  # ...
+}
